a9f24a7b5d05df80059b770480fb6f62548612f3
CoderDan1983 <seekingsomething121@gmail.com>
2022-02-23 07:20:38 -0800

    "initial commit using vs code but not terminal!"

---
+++ main.js
@@ -0,0 +1,20 @@
+// ^
+// ?
+//
+// todo
+// *
+// !
+console.log('main.js, line 7')
+const zippidy = () =>{
+    console.log('main.js, line 9!')
+    if(3  === "dog"){
+        if(3 != 4){
+            
+        }
+    }
+    else{
+        console.log('expected.')
+    }
+}
+
+zippidy();
